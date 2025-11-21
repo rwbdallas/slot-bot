a429c3c93e61b6e2f0adad631c30d3ddc013f58f
rwbdallas <rwbdallas@outlook.com>
2025-11-21 17:43:21 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-TOKEN = "YOUR_DISCORD_TOKEN"
+TOKEN = "MTQ0MDI0NTAxMTQ0MTc3ODgxMg.GxddQJ.p65W5JnMSz4qwTef6Ve8sgiuQ_u_mwfhQoZ5Vw"
 
 
 
 
-RULE_CHANNEL_ID = 1344685721960841226 
+RULE_CHANNEL_ID = 1439639573750611968
 
 EMBED_IMG_URL = "https://cdn.discordapp.com/attachments/1261796252463534130/1262762427611877527/banner_1.png?ex=6697c6a7&is=66967527&hm=85074cee521390841a54481147b384fcfc598903d68ed30a65def5aa154f8a7d&" # embed image url
 
@@ -14,13 +14,13 @@ EMBED_IMG_URL = "https://cdn.discordapp.com/attachments/1261796252463534130/1262
 
 
 
-SlOT_CATEGORYS = [1344359483635142786,1344359512450273311,1344359531257266207] # slot category_1,slot category_2,slot category_3
+SlOT_CATEGORYS = [1441535846971867136,1441536010478424116,1441536035069628436] # slot category_1,slot category_2,slot category_3
 
-SlOT_CATEGORY_1 = 1344359483635142786 # slot category_1 id
+SlOT_CATEGORY_1 = 1441536035069628436
 
-SlOT_CATEGORY_2 = 1344359512450273311 # slot category_2 id
+SlOT_CATEGORY_2 = 1441536010478424116
 
-SlOT_CATEGORY_3 = 1344359531257266207 # slot category_3 id
+SlOT_CATEGORY_3 = 1441535846971867136
 
 
 
@@ -28,9 +28,9 @@ SlOT_CATEGORY_3 = 1344359531257266207 # slot category_3 id
 
 SERVER_ID = 1344685383501221908 # slot server id . Your slot server id 
 
-SELLER_CHANNEL = 1336772337868996618 # seller channel id where will be ping reset message will be sent
+SELLER_CHANNEL = 1439628281921015887 # seller channel id where will be ping reset message will be sent
 
-ROLE_ID = 1344668864834703391 # seller role id the role will be given to the slot create 
+ROLE_ID = 1439621969036447884 # seller role id the role will be given to the slot create 
  
 
 
